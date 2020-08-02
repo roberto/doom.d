13453cb6acb39512d05dcff28d07b791790077bf
Roberto Soares <roberto.tech@gmail.com>
2020-08-01 23:19:45 -0300

    set doom localleader to ,

--- config.el
+++ config.el
@@ -34,6 +34,7 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+(setq doom-localleader-key ",")
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
