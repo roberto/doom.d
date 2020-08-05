a4ca79fc0b3366814ab8a57b9dced37469b08fda
Roberto Soares <roberto.tech@gmail.com>
2020-08-05 15:52:57 -0300

    Group variables definition

--- config.el
+++ config.el
@@ -1,40 +1,16 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(setq doom-localleader-key ","
+      display-line-numbers-type t
+      rustic-lsp-server 'rust-analyzer
 
+      ;; ui
+      doom-theme 'doom-one
+      doom-font (font-spec :family "JetBrains Mono" :size 13)
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Roberto Soares"
-      user-mail-address "")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrains Mono" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-(setq doom-localleader-key ",")
+      ;;  folders
+      org-directory "~/org/"
+      projectile-project-search-path '("~/dev/" "~/dev/nu/"))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
@@ -52,8 +28,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
-
-(setq rustic-lsp-server 'rust-analyzer)
 
 (use-package! evil-terminal-cursor-changer
   :hook (tty-setup . evil-terminal-cursor-changer-activate))
